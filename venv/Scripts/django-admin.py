5c04bd42a73876989a4c8bfbf8ae16389c511633
Ariel1016 <jingga970521@gmail.com>
2020-05-18 01:52:20 +0900

    readme

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\정가현\workspace\python 3.7\django_practice\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
